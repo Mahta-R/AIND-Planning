9cb6bef5e00278fcedfebdd5b3da8c76ff9e1b67
calvinalvin <calvinoh@gmail.com>
2017-12-03 12:14:35 -0800

    Add path for parent to unit test, fixes an issue running tests from root folder

--- tests/test_my_planning_graph.py
+++ tests/test_my_planning_graph.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 parent = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.dirname(parent))
 sys.path.append(os.path.join(os.path.dirname(parent), "aimacode"))
 import unittest
 from aimacode.utils import expr
